a17c10e5b48a2ab338b297667a12a8d2bfd8d536
twinogre <glad2you@naver.com>
2024-01-15 14:22:38 +0900

    first commit

---
+++ src/main/java/com/article/test0115/HomeController.java
@@ -0,0 +1,8 @@
+package com.article.test0115;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+}
---
+++ src/test/java/com/article/test0115/Test0115ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.article.test0115;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test0115ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
